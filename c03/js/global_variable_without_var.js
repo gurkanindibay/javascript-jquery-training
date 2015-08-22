6ea792cce80584f0371f7adf8489ae20f1e0e7a6
gindibay <gurkanindibay@gmail.com>
2015-08-22 05:39:45 +0300

    added files

---
+++ c03/js/global_variable_without_var.js
@@ -0,0 +1,15 @@
+function doMe(){
+
+    a = "123";
+    var b = "321";
+    alert("a:"+a+"  --  b:"+b);
+    b = "something else";
+    alert("a:"+a+"  --  b:"+b);
+    
+};
+
+
+doMe();
+alert("a:"+a+"  --  b:");
+
+//http://jsfiddle.net/gurkanindibay/yn7ybc5f/
